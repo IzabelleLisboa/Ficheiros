19df2ce004cca737eae27f0ab148232ce9f85210
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-10-03 15:59:57 +0100

    Escreve no final do ficheiro

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,7 +1,9 @@
 
 package manipulaficheiros;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,15 +27,54 @@ class Ficheiro {
  }
 
     static void verificaSeExiste() {
+        String nome;
+      System.out.println("Insira o nome do ficheiro que deseja criar");
+      nome = ManipulaFicheiros.ler.nextLine();
+      nome = ManipulaFicheiros.ler.next();
+      File ficheiro = new File (nome);
+      if(!ficheiro.exists()){
+          System.out.println("O ficheiro não existe");
+      }else{
+           System.out.println("O ficheiro existe");
+      }
+        
  }
 
     static void escreveNoFinalFicheiro() {
- }
+      String nome;
+      System.out.println("Insira o nome do ficheiro que deseja criar");
+      nome = ManipulaFicheiros.ler.nextLine();
+      nome = ManipulaFicheiros.ler.next();
+      File ficheiro = new File (nome);
+     
+          try {
+               if(!ficheiro.exists()){
+              ficheiro.createNewFile();
+          }
+          FileWriter fw = new FileWriter(ficheiro,true);
+          BufferedWriter bw = new BufferedWriter(fw);
+          String texto;
+          System.out.println("Insira o conteúdo");
+          texto= ManipulaFicheiros.ler.nextLine();
+          texto= ManipulaFicheiros.ler.nextLine();
+          bw.write(texto);
+          bw.newLine();
+          bw.close();
+          fw.close();
+          
+              } catch (IOException ex) {
+              ex.printStackTrace();
+   }
+          
+      }   
+ 
 
     static void escreveFicheiroNovo() {
+        
  }
 
     static void mostraConteudoFicheiro() {
+        
   }
 
     static void mostraListaFicheiros() {
@@ -44,6 +85,7 @@ class Ficheiro {
             System.out.println(lista[x].getName());
         
     }
- }
-    
+    }
 }
+    
+
